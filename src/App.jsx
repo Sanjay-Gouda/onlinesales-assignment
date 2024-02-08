900be54a984ad435e9c8bc8402c23608031eea0f
Sanjaykumar Gouda <sanjay.kumar@oropocket.com>
2024-02-08 15:44:48 +0530

    update(Heading)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import PreviewForm from "./components/Preview";
 import { FieldContext } from "./context/formFields";
 
 function App() {
-  const { formFields, formData } = useContext(FieldContext);
+  const { formFields } = useContext(FieldContext);
 
   return (
     <>
@@ -13,7 +13,7 @@ function App() {
         <div className="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-              Generate Form
+              Add Dynamic Fields
             </h1>
             <CreateFields />
           </div>
